44965d9169065217b427603e2fbc686b2d0a70b7
Ho-Yon Mak <hoyon.mak@gmail.com>
2018-09-11 23:11:36 +0100

    Parse interface descriptions from protocol XML

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
 #[macro_use]
 extern crate askama;
+extern crate xmltree;
 
 use askama::Template;
 use std::fs::File;
 use std::io::prelude::*;
+use xmltree::Element;
+
+struct Description {
+    full: String,
+    summary: String,
+}
+
+impl Description {
+    fn from_parent(parent: &Element) -> Self {
+        let element = parent.get_child("description").unwrap();
+        Description {
+            full: element.text.as_ref().unwrap().to_string(),
+            summary: element.attributes["summary"].to_string(),
+        }
+    }
+}
+
+struct Interface {
+    name: String,
+    description: Description,
+}
 
 #[derive(Template)]
-#[template(path = "hello.html")]
-struct HelloTemplate<'a> {
-    name: &'a str,
-    age: u32,
+#[template(path = "protocol.html")]
+struct ProtocolTemplate<'a> {
+    interfaces: &'a Vec<Interface>,
 }
 
 fn main() -> std::io::Result<()> {
-    let hello = HelloTemplate { name: "bill" , age: 3};
-    render_to_file(&hello, "site/index.html")?;
+    let file = File::open("./data/xdg-shell.xml")?;
+    let protocol = Element::parse(file).unwrap();
+    let interfaces: Vec<Interface> = protocol
+        .children
+        .into_iter()
+        .filter(|x| x.name == "interface")
+        .map(|i| Interface {
+            name: i.attributes["name"].to_string(),
+            description: Description::from_parent(&i),
+        })
+        .collect();
+
+    let template = ProtocolTemplate {
+        interfaces: &interfaces,
+    };
+    render_to_file(&template, "site/index.html")?;
     Ok(())
 }
 
