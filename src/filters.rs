3823c79e20c1d4ea73fe8960f5ce0de9876a251a
Ho-Yon Mak <hoyon.mak@gmail.com>
2018-09-13 21:00:14 +0100

    complete parser for xml protocol files

--- src/filters.rs
+++ src/filters.rs
@@ -8,14 +8,14 @@ pub fn unwrap(option: &Option<String>) -> Result<&str> {
     }
 }
 
-pub fn desc_full<'a, D: Describable>(describable: &'a D) -> Result<String> {
+pub fn desc_full<D: Describable>(describable: &D) -> Result<String> {
     match describable.get_full() {
         Some(full) => Ok(full),
         None => Ok("".to_string()),
     }
 }
 
-pub fn desc_summary<'a, D: Describable>(describable: &'a D) -> Result<String> {
+pub fn desc_summary<D: Describable>(describable: &D) -> Result<String> {
     match describable.get_summary() {
         Some(summary) => Ok(summary),
         None => Ok("".to_string()),
--- src/protocol.rs
+++ src/protocol.rs
@@ -31,16 +31,22 @@ pub struct Interface {
     pub version: String,
     pub description: Option<Description>,
     pub requests: Vec<Request>,
+    pub events: Vec<Event>,
+    pub enums: Vec<Enum>,
 }
 
 impl Interface {
     pub fn from_element(element: &Element) -> Interface {
         let requests = element_map(element, "request", |r| Request::from_element(&r));
+        let events = element_map(element, "event", |e| Event::from_element(&e));
+        let enums = element_map(element, "enum", |e| Enum::from_element(&e));
         Interface {
             name: element.attributes["name"].to_string(),
             version: element.attributes["version"].to_string(),
             description: Description::from_parent(&element),
             requests,
+            events,
+            enums,
         }
     }
 }
@@ -51,15 +57,106 @@ pub struct Request {
     pub request_type: Option<String>,
     pub since: Option<String>,
     pub description: Option<Description>,
+    pub args: Vec<Arg>,
 }
 
 impl Request {
     pub fn from_element(element: &Element) -> Request {
+        let args = element_map(element, "arg", |a| Arg::from_element(&a));
         Request {
-            name: element.attributes["name"].to_string(),
-            request_type: element.attributes.get("type").map(|s| s.to_string()),
-            since: element.attributes.get("since").map(|s| s.to_string()),
+            name: get_attribute(element, "name"),
+            request_type: get_optional_attribute(element, "type"),
+            since: get_optional_attribute(element, "since"),
             description: Description::from_parent(&element),
+            args,
+        }
+    }
+}
+
+#[derive(Describable)]
+pub struct Event {
+    pub name: String,
+    pub since: Option<String>,
+    pub description: Option<Description>,
+    pub args: Vec<Arg>,
+}
+
+impl Event {
+    pub fn from_element(element: &Element) -> Event {
+        let args = element_map(element, "arg", |a| Arg::from_element(&a));
+        Event {
+            name: get_attribute(element, "name"),
+            since: get_optional_attribute(element, "since"),
+            description: Description::from_parent(&element),
+            args,
+        }
+    }
+}
+
+#[derive(Describable)]
+pub struct Enum {
+    pub name: String,
+    pub since: Option<String>,
+    pub bitfield: Option<String>,
+    pub description: Option<Description>,
+    pub entries: Vec<EnumEntry>,
+}
+
+impl Enum {
+    pub fn from_element(element: &Element) -> Enum {
+        let entries = element_map(element, "entry", |e| EnumEntry::from_element(&e));
+        Enum {
+            name: get_attribute(element, "name"),
+            since: get_optional_attribute(element, "since"),
+            bitfield: get_optional_attribute(element, "bitfield"),
+            description: Description::from_parent(&element),
+            entries,
+        }
+    }
+}
+
+#[derive(Describable)]
+pub struct EnumEntry {
+    pub name: String,
+    pub value: String,
+    pub summary: Option<String>,
+    pub since: Option<String>,
+    pub description: Option<Description>,
+}
+
+impl EnumEntry {
+    pub fn from_element(element: &Element) -> EnumEntry {
+        EnumEntry {
+            name: get_attribute(element, "name"),
+            value: get_attribute(element, "value"),
+            summary: get_optional_attribute(element, "summary"),
+            since: get_optional_attribute(element, "since"),
+            description: Description::from_parent(&element),
+        }
+    }
+}
+
+#[derive(Describable)]
+pub struct Arg {
+    pub name: String,
+    pub arg_type: String,
+    pub summary: Option<String>,
+    pub interface: Option<String>,
+    pub allow_null: Option<String>,
+    pub enum_name: Option<String>,
+    pub description: Option<Description>,
+}
+
+impl Arg {
+    pub fn from_element(element: &Element) -> Arg {
+        Arg {
+            name: get_attribute(element, "name"),
+            arg_type: get_attribute(element, "type"),
+            summary: get_optional_attribute(element, "summary"),
+            interface: get_optional_attribute(element, "interface"),
+            allow_null: get_optional_attribute(element, "allow-null"),
+            enum_name: get_optional_attribute(element, "enum"),
+            description: Description::from_parent(element),
         }
     }
 }
@@ -95,4 +192,12 @@ where
         .filter(|x| x.name == name)
         .map(f)
         .collect()
+}
+
+fn get_attribute(element: &Element, attribute: &str) -> String {
+    element.attributes[attribute].to_string()
+}
+
+fn get_optional_attribute(element: &Element, attribute: &str) -> Option<String> {
+    element.attributes.get(attribute).map(|s| s.to_string())
 }
