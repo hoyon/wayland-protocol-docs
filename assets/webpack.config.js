47a0d1cf5b82497ec9549c85870ce116caa6cfda
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-06-29 23:20:20 +0100

    fix FOUC

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,12 +7,23 @@ module.exports = {
     path: path.resolve(__dirname, '../site'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {hmr: process.env.NODE_ENV === 'development'}
+          },
           { loader: 'css-loader', options: { importLoaders: 1 } },
           'postcss-loader'
         ]
