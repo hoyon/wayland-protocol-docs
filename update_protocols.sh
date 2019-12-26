8a684788ebccc66ee49316face2c5543df32190a
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-12-26 11:49:27 +0000

    add wlroots protocols

--- update_protocols.sh
+++ update_protocols.sh
@@ -8,12 +8,16 @@ mkdir -p wayland
 wget https://gitlab.freedesktop.org/wayland/wayland/raw/master/protocol/wayland.xml -O wayland/wayland.xml
 
 mkdir -p wayland-protocols
+mkdir -p wlroots
 CURRENT_DIR=`pwd`
 DIR=`mktemp -d`
 
 pushd $DIR
 git clone git://anongit.freedesktop.org/wayland/wayland-protocols
 cp -r wayland-protocols/stable wayland-protocols/unstable -t $CURRENT_DIR/wayland-protocols
+
+git clone https://github.com/swaywm/wlr-protocols.git
+cp -r wlr-protocols/unstable -t $CURRENT_DIR/wlroots
 
 popd
 touch ../src/main.rs
